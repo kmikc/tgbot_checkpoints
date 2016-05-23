c6c8f2dd6195dd33609d880b1e5aa404bcd1de7e
jParkes <jparkes@redcalidad.com>
2016-05-23 00:58:02 -0300

    Inicio de la v2 del bot, corriendo en Raspberry Pi

--- bot_ingress_checkpoints.py
+++ bot_ingress_checkpoints.py
@@ -18,12 +18,12 @@ def send_welcome(message):
 #
 # FILTRAR MENSAJES "proximo cp"... etc.
 #
-        
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     logging.basicConfig(filename='bot_ingress_checkpoints.log', filemode='w', level=logging.DEBUG)
     logging.info(message)
-    
+
     text = message.text
     mensajes_cp = [u'proximo cp', u'siguiente cp', u'proximo checkpoint', u'siguiente checkpoint', u'proximo check point', u'siguiente check point']
     mensajes_ciclo = [u'fin de ciclo', u'fin del ciclo', u'final de ciclo', u'final del ciclo', u'proximo ciclo', u'siguiente ciclo', u'ciclo nuevo', u'nuevo ciclo']
@@ -33,38 +33,38 @@ def echo_all(message):
     if len(revise_message) > 0:
         _init_cycle = datetime.strptime('2015-06-24 07:00', '%Y-%m-%d %H:%M')
         _now = datetime.now() - timedelta(hours=4)
-        
+
         messages = []
         revise_ciclo = [find_text for find_text in mensajes_ciclo if find_text in text.lower()]
-        
+
         if len(revise_ciclo) > 0:
-            
-            ding_ding_ding = False
-            while (not ding_ding_ding):
+
+            _match = False
+            while (not _match):
                 _end_cycle = _init_cycle + timedelta(hours=175)
                 if (_end_cycle < _now):
                     _init_cycle = _end_cycle
                 else:
-                    ding_ding_ding = True
-                
+                    _match = True
+
             messages.append('El final de este ciclo es en:\n{0}'.format(str(_end_cycle)))
-                
+
         revise_cp = [find_text for find_text in mensajes_cp if find_text in text.lower()]
-        
+
         if len(revise_cp) > 0:
-            
-            ding_ding_ding = False
-            while (not ding_ding_ding):
+
+            _match = False
+            while (not _match):
                 _end_cycle = _init_cycle + timedelta(hours=5)
                 if (_end_cycle < _now):
                     _init_cycle = _end_cycle
                 else:
-                    ding_ding_ding = True
-                    
+                    _match = True
+
             messages.append('El proximo CheckPoint es en:\n{0}'.format(str(_end_cycle)))
-            
+
         msg = ", ".join(messages)
-        
+
         _respuesta = msg.encode('utf-8')
         bot.reply_to(message, _respuesta)
 
